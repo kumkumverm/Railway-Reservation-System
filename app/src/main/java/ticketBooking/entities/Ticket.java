5429433eb03553ba253fd5244eec068e9a837937
Kumkum <kumkumverma101@gmail.com>
2025-09-11 14:16:55 +0530

    Make the git repo and add some code

---
+++ app/src/main/java/ticketBooking/entities/Ticket.java
@@ -0,0 +1,12 @@
+package ticketBooking.entities;
+
+import java.sql.Date;
+
+public class Ticket {
+    private String ticketId;
+    private String userId;
+    private String source;
+    private String destination;
+    private Date dateOfTravel;
+    private Train train;
+}
---
+++ app/src/main/java/ticketBooking/entities/Train.java
@@ -0,0 +1,12 @@
+package ticketBooking.entities;
+
+import java.sql.Time;
+import java.util.List;
+import java.util.Map;
+
+public class Train {
+    private String trainId;
+    private String trainNo;
+    private List<List<Integer>> seats;
+    private Map<String,Time> stationTime;
+}
