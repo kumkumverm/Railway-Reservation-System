e4f91943cfdfdb30badca501ee6ae7072bff673d
Kumkum <kumkumverma101@gmail.com>
2025-09-11 19:22:25 +0530

    add user with getters setters and printTickets methods

--- app/src/main/java/ticketBooking/entities/User.java
+++ app/src/main/java/ticketBooking/entities/User.java
@@ -8,4 +8,50 @@ public class User {
     private String hashPassword;
     private List<Ticket> ticketsBooked;
     private String userId;
+    public User(String name,String password,String hashedPassword,List<Ticket> ticketsBooked,String userId){
+        this.name=name;
+        this.password=password;
+        this.hashPassword=hashedPassword;
+        this.ticketsBooked=ticketsBooked;
+        this.userId=userId;
+    }
+    public User(){
+
+    }
+    public String getName() {
+        return name;
+    }
+    public void setName(String name) {
+        this.name = name;
+    }
+    public String getPassword() {
+        return password;
+    }
+    public void setPassword(String password) {
+        this.password = password;
+    }
+    public String getHashPassword() {
+        return hashPassword;
+    }
+    public void setHashPassword(String hashPassword) {
+        this.hashPassword = hashPassword;
+    }
+    public List<Ticket> getTicketsBooked() {
+        return ticketsBooked;
+    }
+    public void setTicketsBooked(List<Ticket> ticketsBooked) {
+        this.ticketsBooked = ticketsBooked;
+    }
+    public String getUserId() {
+        return userId;
+    }
+    public void setUserId(String userId) {
+        this.userId = userId;
+    }
+    public void printTickets(){
+        for(int i=0;i<ticketsBooked.size();i++){
+
+            System.out.println(ticketsBooked.get(i).getTicketInfo());
+        }
+    }
 }
